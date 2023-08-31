68baef5c562c48d648d04d65505a4c6a4a60c090
KumaCatz <32141751+KumaCatz@users.noreply.github.com>
2023-08-31 20:43:05 +0300

    .

--- src/app/search/components/SearchBar.tsx
+++ src/app/search/components/SearchBar.tsx
@@ -1,5 +1,17 @@
 export default function SearchBar() {
   return(
-    <div>Search Bar</div>
+    <>
+    <div>Basic Search</div>
+      <label htmlFor="pet">types of pets available:</label>
+      <select name='pet' id='pet'>
+        <option value="thing">thing</option>
+        types of pets available list
+      </select>
+    <div>Advanced Search</div>
+      <label htmlFor="status">Status:</label>
+      <input type='checkbox' name='fostered' />fostered
+      <input type='checkbox' name='adopted' />adopted
+      <input type='checkbox' name='available' />available      
+    </>
   )
 }
